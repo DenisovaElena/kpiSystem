2103fc30f16acf3a9e282460f4266983912ce7f9
Loki <khalilova@mail.com>
2019-11-20 15:20:17 +0300

    Создание кнопки создания и удаления эелементов

--- src/main/webapp/resources/js/front-jsplimb2.js
+++ src/main/webapp/resources/js/front-jsplimb2.js
@@ -37,7 +37,9 @@ jsPlumb.ready(function () {
             var initNode = function(el) {
                     instance.draggable(el); // initialise draggable elements.
                     instance.makeSource(el, {
+                        isSource: true,
                         filter: ".ep",
+                        //createEndpoint:true,
                         anchor: "Continuous",
                         connectorStyle: { stroke: "#5c96bc", strokeWidth: 3, outlineStroke: "transparent", outlineWidth: 4 },
                         connectionType:"basic",
@@ -48,12 +50,17 @@ jsPlumb.ready(function () {
                         onMaxConnections: function (info, e) {
                             alert("Maximum connections (" + info.maxConnections + ") reached");
                         }
+
+
                     });
                     instance.makeTarget(el, {
                         isTarget:true,
                         dropOptions: { hoverClass: "dragHover" },
+                        createEndpoint:true,
                         anchor: "Continuous",
                         allowLoopback: false //Предотвращение петлевых подключений
+
+
                     });
             };
 // создание нового элемента.
@@ -62,7 +69,9 @@ jsPlumb.ready(function () {
                     var id = jsPlumbUtil.uuid();
                     d.className = "w";
                     d.id = id;
-                    d.innerHTML = id.substring(0, 8) /*подпись блока*/ + "<div class='ep'></div>" + "<div class='main-btn1'></div>";
+                    function name() {
+                    }
+                    //d.innerHTML = id.substring(0, 8) /*подпись блока*/ + "<div class='ep'></div>" + "<div class='main-btn1'></div>";
                     d.style.left = 0 + "px";
                     d.style.top = 0 + "px";
                     instance.getContainer().appendChild(d);
@@ -96,15 +105,7 @@ jsPlumb.ready(function () {
             instance.addToGroup("aGroup", division4);
 
 
-            var endpointOptions = {
-                anchor:"division1",
-                isSource:true,
-                isTarget:true,
-                connector : "Straight",
-                connectorStyle: { lineWidth:20, strokeStyle:'blue' },
-                scope:"blueline",
-                dragAllowedWhenFull:false
-            };
+
             instance.addGroup({
                 el: branch1,
                 id:"bGroup",
@@ -123,7 +124,7 @@ jsPlumb.ready(function () {
             instance.addToGroup("bGroup", branch3);
             instance.addToGroup("bGroup", branch4);
 // Соединители
-            instance.connect({ source: "division1", target: "branch3", type:"basic" });
+            instance.connect({ source: "division1", target: "branch3", type:"basic"});
             instance.connect({ source: "division1", target: "branch2", type:"basic"
             });
 // collapse/expand group button
@@ -134,7 +135,6 @@ jsPlumb.ready(function () {
                 instance[collapsed ? "expandGroup" : "collapseGroup"](g);
             });
             jsPlumb.fire("jsPlumbDemoLoaded", instance);
-
 // Модальное окно
     var modal = document.getElementById('myModal');
     var btn= document.getElementById('myBtn');
@@ -156,23 +156,181 @@ jsPlumb.ready(function () {
     $('.main-btn').click(function(e) {
         newNode(e.offsetX, e.offsetY);
         modal.style.display = "none";
+        d.innerHTML = id.substring(0, 8) /*подпись блока*/ + "<div class='ep'></div>" + "<div class='main-btn1'></div>";
     });
 
 
 // Кнопка удаления
-    $('.main-btn1').click(function (e) {
-        var g = $(this).parent().remove();
+    $('.main-btn1').on('click', function (c) {
+        var g= $(this).parent().remove();
+
+        // var conn = g.getConnections({
+        //     source: sourceId,
+        //     target: targetId
+        // });
+        // if (conn[0]) {
+        //     instance.deleteConnection(conn[0]);
+       // instance.bind("click", function (c) {
+       //      instance.deleteConnection(c);
+       //  });
+
+      //  $(this).parent().remove();
+     //  $(this).getConnections().hide();
+    });
+
+        //$(this).deleteConnection(c);
+        //jsPlumb.deleteEveryEndpoint();
+
+
+        //jsPlumb.deleteConnectionsForElement(el, [params])
         /*
-        var conn = g.getConnections({
-            source: sourceId,
-            target: targetId
+                /*
+                var conn = g.getConnections({
+                    source: sourceId,
+                    target: targetId
+                });
+                if (conn[0]) {
+                    instance.detach(conn[0]);
+                }
+                 */
+});
+$(function(){
+
+    $('.load-more').on('click', function(){
+        const btn = $(this);
+        const loader = btn.find('span');
+        $.ajax({
+            url: '/data.html',
+            type: 'GET',
+            beforeSend: function(){
+                btn.attr('disabled', true);
+                loader.addClass('d-inline-block');
+            },
+            success: function(responce){
+                setTimeout(function(){
+                    loader.removeClass('d-inline-block');
+                    btn.attr('disabled', false);
+                    $('.after-posts').before(responce);
+                    ///console.log(responce);
+                }, 1000);
+            },
+            error: function() {
+                alert('Error!');
+                loader.removeClass('d-inline-block');
+                btn.attr('disabled', false);
+            }
+
         });
-        if (conn[0]) {
-            instance.detach(conn[0]);
-        }
-         */
     });
 });
+
+
+
+
+
+/*
+
+//Начало работы
+    $(document).ready(function() {         //обработчик события, выполнить функцию, когда документ загрузится
+        //alert($('div', '.grid_6').html()); //содержание страницы, второй параметр - родитель
+        var a = $('<h5>Hello</h5>')
+        $('div', '.grid_6').append(a);      //добавляет к существующему div строку Hello
+    });
+//начало работы сейчас - в качестве параметра функция
+    $(function() {
+//Селекторы - строчные выражения, с помощью которых задаются условия поиска
+
+//Базовые селекторы
+//селектор по классу
+    $('*', 'grid_5');
+    all.css({'border': '2px'});
+    //* - библиотека выберет все элементы данной страницы и вернет объект множества выбранных элементов
+    //правило css определяет свойства для выбранных элементов
+    $('*', '.grid_5.preffix_1');    // через точку перечисляет, через пробел ищем вложенные элементы
+    $('#id');                       //селекторы по идентификатору
+    $('#id, span, .grid.prefix');   //селекторы по тегу, через запятую перечисляем все элементы
+    });
+//селекторы иерархии
+    $('.container>div').css({'border': '2px'}); //выбирает прямыъ потомков контейнера (у каждого конте йнера только один потомок
+    $('.div.grid_12+div').css({'border': '2px'}); //выбирает следующий элемент (div) за контейнером
+    $('.div.grid_12 ~ div').css({'border': '2px'}); //выбирает все следующие элементы (div) за контейнером\
+
+    var obj = $('dev.grid_12 ~ div');  //в obj сейчас два элемента, те, которые следуют за dev.grid_12
+    alert(obj[0]);                      //выводим содежимое первого объекта
+    obj[0].innerHTML = 'Hello!'          //заменить содержимое элемента obj[0]
+
+    for(v in obj) {
+        document.write("<strong>" + v + "-</strong>" + obj[v] + "<br />")
+    }
+//селекторы по атрибутам
+    $('[src]').css({'border': '2px'});  //при работе с атрибутами оборачивать в []
+    $('div[name=email]').css({'border': '2px'}); // поиск всех блоков div с атрибутом name
+    $('div[name!=value]').css({'border': '2px'});
+    $('div[name^=value]').css({'border': '2px'});
+    $('div[name$=value]').css({'border': '2px'});
+    $('div[name*=value]').css({'border': '2px'});
+    $('div[name~=value]').css({'border': '2px'});
+    $('div[name |=value]').css({'border': '2px'});
+    $('div[name*=value]').css({'border': '2px'});
+//Методы выбора элементов
+    $('.inner').parent().addClass('selected'); //выбор родительских элементов
+    $('.block').parent().addClass('selected'); //выбора дочерних элементов
+    $('li#second-list-item').closest('ul').addClass('selected'); //ближайший
+    $('.inner').find('h4').addClass('selected'); //находит по идентификатору
+    $('.inner').prev('h4').addClass('selected'); //находит по идентификатору
+    $('.inner').next('h4').addClass('selected'); //находит по идентификатору
+    $('.inner').siblings('h4').addClass('selected'); //находит по идентификатору
+//Методы события
+/*
+.click()
+.dblclock();
+.mouseclick(); наведение мыши на кнопку
+.mouseleave(); уведение мыши с кнопки
+.focus();
+.blur(); //размытие, клик вне выделенной области
+.change(); // изменение текста в области
+.text(); //добавление текста
+.change();
+.keyup(); //нажатие клавиши
+ */
+/*
+$('#button-1').click(function() {
+    $('#top-header').toggleClass('pinkColor');  //смена цвета при клике
+})
+
+//.on - Этот метод быстрее, не загружает память и работает с теми элементами, которые были добавлены позже
+$('#button-1').on('click', function() {
+    $('#top-header').toggleClass('pinkColor');  //смена цвета при клике
+    $('#top-header').addClass('pinkColor');
+})
+
+//добавление текста
+$('#inputName').on('change', function() {
+    $('#textHolder').text($('#this').val());
+});
+
+//реакция на нажатие клавиши
+$('#inputName').on('keyup', function(event){
+    if (event.which = 27) {
+        //alert('it was ESC!');
+        $('#textHolder').text('');
+    }
+});
+//методы работы с классами
+$('#customCSS').on('clock', function(){
+    $('#textBox').css({'border': '2px'})
+});
+
+*/
+
+
+
+
+
+
+
+
+
 
 
 
