9c5bcea0ccabcb14fec5a4c1b2ac022d49c49839
ZhukovaSD <skantysheva@gmail.com>
2019-11-20 16:54:55 +0300

    add DivisionPage.html

--- src/main/webapp/resources/js/divisionsUtil.js
+++ src/main/webapp/resources/js/divisionsUtil.js
@@ -14,7 +14,18 @@ $.ajax({
         $('input#patronym').val(data.chiefEmployee.patronym);
         $('input#position').val(data.chiefEmployee.position);
         $('input#email').val(data.chiefEmployee.email);
-        $('input#funcN1').val(data.chiefEmployee.funcN1);
     }
+});
 
+$.ajax({
+    url: "rest/profile/authorities/getAuthoritiesByDivisionId/" + getId(),
+    method: "GET",
+    contentType: "application/json",
+    success: function(data) {
+        console.log(data); // Возвращаемые данные выводим в консоль
+        $('input#funcN1').val(data.name);
+        $('input#funcN2').val(data.authorities.funcN2);
+
+        $('input#column1').val(data.authorities.column1);
+    }
 });
