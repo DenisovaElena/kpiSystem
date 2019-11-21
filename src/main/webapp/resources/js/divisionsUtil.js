82997a622f82905a4b67ead824ab67114c45e6f9
ZhukovaSD <skantysheva@gmail.com>
2019-11-21 16:23:01 +0300

    add DivisionPage1.html

--- src/main/webapp/resources/js/divisionsUtil.js
+++ src/main/webapp/resources/js/divisionsUtil.js
@@ -2,34 +2,17 @@ function getId() {
     return new URL(window.location.href).searchParams.get("id");
 }
 
-function addElements (url) {
+function addElement (url, block, idName) {
     return $.getJSON(url, function(data) {
         for(var i in data) {
             var key = parseInt(i)+1;
-            $('#functionBlock').append(
+            $(block).append(
                 '<div class="row d-flex align-items-center mb-3">' +
                 '   <div class="col-3 text-right">' +
-                '       <label for="funcN'+key+'">'+key+'.</label>' +
+                '       <label for="'+idName+key+'">'+key+'.</label>' +
                 '   </div>' +
                 '   <div class="col-9">' +
-                '       <input type="text" class="form-control" id="funcN'+key+'" name="funcN'+key+'" value="'+data[i].name+'">' +
-                '   </div>' +
-                '</div>'
-            );
-        }
-    });
-}
-function addElements (url) {
-    return $.getJSON(url, function(data) {
-        for(var i in data) {
-            var key = parseInt(i)+1;
-            $('#functionBlock').append(
-                '<div class="row d-flex align-items-center mb-3">' +
-                '   <div class="col-3 text-right">' +
-                '       <label for="funcN'+key+'">'+key+'.</label>' +
-                '   </div>' +
-                '   <div class="col-9">' +
-                '       <input type="text" class="form-control" id="funcN'+key+'" name="funcN'+key+'" value="'+data[i].name+'">' +
+                '       <input type="text" class="form-control" id="'+idName+key+'" name="'+idName+key+'" value="'+data[i].name+'">' +
                 '   </div>' +
                 '</div>'
             );
@@ -37,6 +20,23 @@ function addElements (url) {
     });
 }
 
+function addUsers (url, block) {
+    return $.getJSON(url, function(data) {
+        for(var i in data) {
+            var key = parseInt(i)+1;
+            $(block).append(
+                '<div class="row d-flex align-items-center justify-content-center text-center" >' +
+                '   <div class="col-1 border-right border-bottom my-0 py-2">'+key+'</div>' +
+                '   <div class="col-2 border-right border-bottom my-0 py-2">'+data[i].lastname+'</div>' +
+                '   <div class="col-2 border-right border-bottom my-0 py-2">'+data[i].firstname+'</div>' +
+                '   <div class="col-3 border-right border-bottom my-0 py-2">'+data[i].patronym+'</div>' +
+                '   <div class="col-2 border-right border-bottom my-0 py-2">'+data[i].position+'</div>' +
+                '   <div class="col-2 border-bottom my-0 py-2">'+data[i].email+'</div>' +
+                '</div>'
+            );
+        }
+    });
+}
 
 $.ajax({
     url: "rest/profile/divisions/" + getId(),
@@ -54,7 +54,9 @@ $.ajax({
 });
 
 
-addElements('rest/profile/authorities/getAuthoritiesByDivisionId/' + getId());
+addElement('rest/profile/authorities/getAuthoritiesByDivisionId/' + getId(), '#functionBlock', 'funcN');
+addElement('rest/profile/divisions/getChildDivisionByDivisionId/' + getId(), '#childDivisionBlock', 'column');
+addUsers('rest/profile/divisions/employees/getEmployeesByDivisionId' + getId(), '#employeesDivisionBlock');
 /*
 $.ajax({
     url: "rest/profile/authorities/getAuthoritiesByDivisionId/" + getId(),
