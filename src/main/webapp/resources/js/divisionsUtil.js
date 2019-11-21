e507ad0757b5346cde682724167703a95430b804
DenisovaEN <dummy.newmailbox@gmail.com>
2019-11-21 13:12:46 +0300

    web///

--- src/main/webapp/resources/js/divisionsUtil.js
+++ src/main/webapp/resources/js/divisionsUtil.js
@@ -2,12 +2,48 @@ function getId() {
     return new URL(window.location.href).searchParams.get("id");
 }
 
+function addElements (url) {
+    return $.getJSON(url, function(data) {
+        for(var i in data) {
+            var key = parseInt(i)+1;
+            $('#functionBlock').append(
+                '<div class="row d-flex align-items-center mb-3">' +
+                '   <div class="col-3 text-right">' +
+                '       <label for="funcN'+key+'">'+key+'.</label>' +
+                '   </div>' +
+                '   <div class="col-9">' +
+                '       <input type="text" class="form-control" id="funcN'+key+'" name="funcN'+key+'" value="'+data[i].name+'">' +
+                '   </div>' +
+                '</div>'
+            );
+        }
+    });
+}
+function addElements (url) {
+    return $.getJSON(url, function(data) {
+        for(var i in data) {
+            var key = parseInt(i)+1;
+            $('#functionBlock').append(
+                '<div class="row d-flex align-items-center mb-3">' +
+                '   <div class="col-3 text-right">' +
+                '       <label for="funcN'+key+'">'+key+'.</label>' +
+                '   </div>' +
+                '   <div class="col-9">' +
+                '       <input type="text" class="form-control" id="funcN'+key+'" name="funcN'+key+'" value="'+data[i].name+'">' +
+                '   </div>' +
+                '</div>'
+            );
+        }
+    });
+}
+
+
 $.ajax({
     url: "rest/profile/divisions/" + getId(),
     method: "GET",
     contentType: "application/json",
     success: function(data) {
-        console.log(data); // Возвращаемые данные выводим в консоль
+        //console.log(data); // Возвращаемые данные выводим в консоль
         $('input#nameOtdel').val(data.name);
         $('input#firstName').val(data.chiefEmployee.firstname);
         $('input#lastName').val(data.chiefEmployee.lastname);
@@ -17,15 +53,15 @@ $.ajax({
     }
 });
 
+
+addElements('rest/profile/authorities/getAuthoritiesByDivisionId/' + getId());
+/*
 $.ajax({
     url: "rest/profile/authorities/getAuthoritiesByDivisionId/" + getId(),
     method: "GET",
     contentType: "application/json",
     success: function(data) {
         console.log(data); // Возвращаемые данные выводим в консоль
-        $('input#funcN1').val(data.name);
-        $('input#funcN2').val(data.authorities.funcN2);
-
-        $('input#column1').val(data.authorities.column1);
+        $('input#funcN1').val(data[0].name);
     }
-});
+});*/
