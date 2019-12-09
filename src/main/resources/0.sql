6a616427d078557ea1b086ac3fe964d86f041832
StanislavMakhrov <SlavaM4@yandex.ru>
2019-12-09 08:53:31 +0300

    JsonIgnore deleted for Authorities at Divisions

---
+++ src/main/resources/0.sql
@@ -0,0 +1,4 @@
+truncate table kpi.authority CASCADE;
+truncate table kpi.authority_child_authorities CASCADE;
+truncate table kpi.authority_divisions CASCADE;
+truncate table kpi.division_child_division CASCADE;
